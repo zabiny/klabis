536d00a45b31734a773d141091ce2e98b17c429e
David Polach <d.polach@dpolach.com>
2024-05-19 23:14:48 +0200

    added springdoc configuration

--- backend/src/main/java/club/klabis/adapters/api/ApiSecurityConfiguration.java
+++ backend/src/main/java/club/klabis/adapters/api/ApiSecurityConfiguration.java
@@ -1,14 +1,18 @@
 package club.klabis.adapters.api;
 
 import club.klabis.config.authserver.AuthorizationServerConfiguration;
+import club.klabis.config.authserver.SecurityConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 @Configuration(proxyBeanMethods = false)
@@ -18,7 +22,7 @@ public class ApiSecurityConfiguration {
     @Order(value = AuthorizationServerConfiguration.AFTER_AUTH_SERVER_SECURITY_ORDER)
     public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .securityMatcher(new AntPathRequestMatcher("/api/**"))
+                .securityMatcher(new NegatedRequestMatcher(SecurityConfiguration.UI_REQUESTS_MATCHER))
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
--- backend/src/main/java/club/klabis/config/authserver/SecurityConfiguration.java
+++ backend/src/main/java/club/klabis/config/authserver/SecurityConfiguration.java
@@ -4,18 +4,14 @@ import club.klabis.config.authserver.sociallogin.SocialLoginAuthenticationSucces
 import club.klabis.config.authserver.sociallogin.RegisterMemberFromSocialLoginHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
-import org.springframework.security.web.util.matcher.OrRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.security.web.util.matcher.*;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -23,13 +19,15 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration(proxyBeanMethods = false)
 public class SecurityConfiguration {
 
+    public static RequestMatcher UI_REQUESTS_MATCHER = new AndRequestMatcher(new MediaTypeRequestMatcher(MediaType.TEXT_HTML), new NegatedRequestMatcher(new MediaTypeRequestMatcher(MediaType.ALL)));
+
     @Bean
     @Order(value = AuthorizationServerConfiguration.AFTER_AUTH_SERVER_SECURITY_ORDER)
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http,
                                                           AuthenticationSuccessHandler authenticationSuccessHandler
     ) throws Exception {
         return http
-                .securityMatcher(new NegatedRequestMatcher(new AntPathRequestMatcher("/api/**")))
+                .securityMatcher(UI_REQUESTS_MATCHER)
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
                 .formLogin(withDefaults())
                 .oauth2Login(oauth -> oauth.successHandler(authenticationSuccessHandler))
