76f65b3013f55679b7e1440aed756508c900a588
David Polach <d.polach@dpolach.com>
2024-06-08 13:53:12 +0200

    Mapper testing support

---
+++ backend/src/test/java/club/klabis/tests/common/MapperTestConfiguration.java
@@ -0,0 +1,10 @@
+package club.klabis.tests.common;
+
+import club.klabis.common.MapperSpringConfig;
+import org.mapstruct.extensions.spring.test.ConverterScan;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration
+@ConverterScan(basePackageClasses = MapperSpringConfig.class)
+class MapperTestConfiguration {
+}
