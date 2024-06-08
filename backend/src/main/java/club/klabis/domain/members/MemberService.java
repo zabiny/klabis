330a2b18a9edb6d638c3beb37c7bc4bfd91a6323
David Polach <d.polach@dpolach.com>
2024-06-08 13:53:36 +0200

    Validace registracniho formulare

--- backend/src/main/java/club/klabis/domain/members/MemberService.java
+++ backend/src/main/java/club/klabis/domain/members/MemberService.java
@@ -1,11 +1,14 @@
 package club.klabis.domain.members;
 
 import club.klabis.domain.members.forms.RegistrationForm;
+import jakarta.validation.Valid;
 import org.jmolecules.ddd.annotation.Service;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 import java.util.Optional;
 
+@Validated
 @Service
 public interface MemberService {
 
@@ -13,7 +16,7 @@ public interface MemberService {
 
     Optional<Member> findByGoogleSubject(String googleSub);
 
-    Member registerMember(RegistrationForm registrationForm);
+    Member registerMember(@Valid RegistrationForm registrationForm);
 
     List<Member> findAll(boolean includeSuspended);
 }
--- backend/src/main/java/club/klabis/domain/members/forms/RegistrationForm.java
+++ backend/src/main/java/club/klabis/domain/members/forms/RegistrationForm.java
@@ -2,24 +2,34 @@ package club.klabis.domain.members.forms;
 
 import club.klabis.domain.members.*;
 import io.soabase.recordbuilder.core.RecordBuilder;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.List;
 
+// TODO : dodelat extended validace - see https://github.com/orgs/zabiny/projects/1/views/1?pane=issue&itemId=39979105
 @RecordBuilder
 public record RegistrationForm(
+        @NotBlank
         String firstName,
+        @NotBlank
         String lastName,
+        @NotNull
         Sex sex,
+        @NotNull
         LocalDate dateOfBirth,
         String birthCertificateNumber,
+        @NotNull
         String nationality,
+        @NotNull
         Address address,
         Collection<Contact> contact,
-        List<LegalGuardian> guardians,
+        Collection<LegalGuardian> guardians,
         String siCard,
         String bankAccount,
+        @NotNull
         RegistrationNumber registrationNumber,
         Integer orisId
 ) {
