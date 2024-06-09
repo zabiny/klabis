9cf848bd5f8b1fb065487134b52aaa2181b41faa
David Polach <d.polach@dpolach.com>
2024-06-09 14:55:51 +0200

    cleanup

--- backend/src/main/java/club/klabis/domain/members/RefereeLicenceType.java
+++ backend/src/main/java/club/klabis/domain/members/RefereeLicenceType.java
@@ -2,9 +2,6 @@ package club.klabis.domain.members;
 
 public enum RefereeLicenceType {
     R1,
-
     R2,
-
     R3;
-
 }
--- backend/src/main/java/club/klabis/domain/members/TrainerLicence.java
+++ backend/src/main/java/club/klabis/domain/members/TrainerLicence.java
@@ -1,6 +1,9 @@
 package club.klabis.domain.members;
 
+import org.jmolecules.ddd.annotation.ValueObject;
+
 import java.time.LocalDate;
 
+@ValueObject
 public record TrainerLicence(TrainerLicenceType type, LocalDate expiryDate) {
 }
