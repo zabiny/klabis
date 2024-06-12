1e5c3a978c726466bf327b6439b752f61e5aba6a
David Polach <d.polach@dpolach.com>
2024-06-13 00:31:37 +0200

    response of POST /memberRegistration contains headers with location of created member and member id

--- backend/src/main/java/club/klabis/adapters/api/MembersRegistrationController.java
+++ backend/src/main/java/club/klabis/adapters/api/MembersRegistrationController.java
@@ -2,11 +2,14 @@ package club.klabis.adapters.api;
 
 import club.klabis.api.MemberRegistrationsApi;
 import club.klabis.api.dto.MemberRegistrationFormApiDto;
+import club.klabis.domain.members.Member;
 import club.klabis.domain.members.MemberService;
 import club.klabis.domain.members.forms.RegistrationForm;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 public class MembersRegistrationController implements MemberRegistrationsApi {
@@ -21,8 +24,8 @@ public class MembersRegistrationController implements MemberRegistrationsApi {
 
     @Override
     public ResponseEntity<Void> memberRegistrationsPost(MemberRegistrationFormApiDto memberRegistrationFormApiDto) {
-        service.registerMember(conversionService.convert(memberRegistrationFormApiDto, RegistrationForm.class));
-        return ResponseEntity.ok(null);
+        Member createdMember = service.registerMember(conversionService.convert(memberRegistrationFormApiDto, RegistrationForm.class));
+        return ResponseEntity.created(URI.create("/members/%s".formatted(createdMember.getId()))).header("MemberId", "%d".formatted(createdMember.getId())).build();
     }
 
 }
