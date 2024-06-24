aca5cfe9e88b5d5971e72174423883c2c12ce2fa
David Polach <d.polach@dpolach.com>
2024-06-25 00:13:11 +0200

    Removal of unused classes from auth server

--- backend/src/main/java/club/klabis/config/authserver/socialloginsupport/GoogleOidcUserToKlabisSocialLoginOidcUserMapper.java
+++ backend/src/main/java/club/klabis/config/authserver/socialloginsupport/GoogleOidcUserToKlabisSocialLoginOidcUserMapper.java
@@ -1,51 +1,17 @@
 package club.klabis.config.authserver.socialloginsupport;
 
-import club.klabis.config.authserver.KlabisOidcUser;
 import club.klabis.domain.appusers.ApplicationUser;
 import club.klabis.domain.appusers.ApplicationUsersRepository;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.core.oidc.OidcIdToken;
-import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
-import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.Optional;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Component
 class GoogleOidcUserToKlabisSocialLoginOidcUserMapper implements SocialLoginOidcUserToKlabisOidcUserMapper {
     @Override
     public String getRegistration() {
         return "google";
-    }
-
-    @Override
-    public KlabisOidcUser map(OidcIdToken idToken, OidcUserInfo userInfo, ApplicationUser user, List<String> roles) {
-        Set<GrantedAuthority> authorities = roles.stream()
-                .map(roleName -> new SimpleGrantedAuthority(roleName))
-                .collect(Collectors.toSet());
-
-        Map<String, Object> claims = new HashMap<>();
-        //claims.putAll(idToken.getClaims());
-        claims.put(StandardClaimNames.SUB, user.getUsername());
-//        claims.put(StandardClaimNames.GIVEN_NAME, user.getFirstName());
-//        claims.put(StandardClaimNames.MIDDLE_NAME, user.getMiddleName());
-//        claims.put(StandardClaimNames.FAMILY_NAME, user.getLastName());
-//        claims.put(StandardClaimNames.LOCALE, user.getLocale());
-//        claims.put(StandardClaimNames.PICTURE, user.getAvatarUrl());
-
-        OidcIdToken customIdToken = new OidcIdToken(
-                idToken.getTokenValue(), idToken.getIssuedAt(), idToken.getExpiresAt(), claims
-        );
-
-        KlabisOidcUser oidcUser = new KlabisOidcUser(authorities, customIdToken, userInfo);
-//        oidcUser.setId(user.getId());
-//        oidcUser.setUsername(user.getUsername());
-//        oidcUser.setCreatedAt(user.getCreatedAt());
-//        oidcUser.setActive(user.isActive());
-        return oidcUser;
     }
 
     @Override
