3fdb1c659bea1f8fcdb9589ba9ab9a5246bef19f
David Polach <d.polach@dpolach.com>
2025-02-02 22:11:44 +0100

    GET /members supports hal+json and klabis+json responses

---
+++ backend/src/main/java/club/klabis/adapters/api/members/mappers/BaseMemberMapper.java
@@ -0,0 +1,49 @@
+package club.klabis.adapters.api.members.mappers;
+
+import club.klabis.adapters.api.KlabisSecurityService;
+import club.klabis.adapters.api.members.MembersController;
+import club.klabis.api.dto.MemberApiDto;
+import club.klabis.domain.appusers.ApplicationGrant;
+import club.klabis.domain.members.Member;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.MappingTarget;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+public abstract class BaseMemberMapper<T extends RepresentationModel<T>> extends RepresentationModelAssemblerSupport<Member, T> implements Converter<Member, T> {
+    private KlabisSecurityService securityService;
+
+    public BaseMemberMapper(Class<?> controllerClass, Class<T> resourceType) {
+        super(controllerClass, resourceType);
+    }
+
+    @AfterMapping
+    void assembleLinks(Member entity, @MappingTarget T target) {
+        target.add(linkTo(methodOn(MembersController.class).membersMemberIdGet(entity.getId())).withSelfRel());
+
+        if (securityService.canEditMemberData(entity.getId())) {
+            target.add(linkTo(methodOn(MembersController.class).membersMemberIdEditOwnMemberInfoFormGet(entity.getId())).withRel(
+                    "members:editOwnInfo"));
+        }
+
+        if (securityService.hasGrant(ApplicationGrant.MEMBERS_EDIT)) {
+            target.add(linkTo(methodOn(MembersController.class).getMemberEditByAdminForm(entity.getId())).withRel(
+                    "members:editAnotherMember"));
+        }
+    }
+
+    @Autowired
+    public void setSecurityService(KlabisSecurityService securityService) {
+        this.securityService = securityService;
+    }
+
+    @Override
+    public final T convert(Member source) {
+        return toModel(source);
+    }
+}
--- backend/src/main/java/club/klabis/adapters/api/members/mappers/MemberCompactViewMapper.java
+++ backend/src/main/java/club/klabis/adapters/api/members/mappers/MemberCompactViewMapper.java
@@ -1,26 +1,15 @@
 package club.klabis.adapters.api.members.mappers;
 
-import club.klabis.adapters.api.KlabisSecurityService;
 import club.klabis.adapters.api.members.MembersController;
 import club.klabis.api.dto.MemberViewCompactApiDto;
 import club.klabis.common.mapstruct.DomainToDtoMapperConfiguration;
-import club.klabis.domain.appusers.ApplicationGrant;
 import club.klabis.domain.members.Member;
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Mapper(config = DomainToDtoMapperConfiguration.class)
-public abstract class MemberCompactViewMapper extends RepresentationModelAssemblerSupport<Member, MemberViewCompactApiDto> implements Converter<Member, MemberViewCompactApiDto> {
-
-    private KlabisSecurityService securityService;
+public abstract class MemberCompactViewMapper extends BaseMemberMapper<MemberViewCompactApiDto> implements Converter<Member, MemberViewCompactApiDto> {
 
     public MemberCompactViewMapper() {
         super(MembersController.class, MemberViewCompactApiDto.class);
@@ -29,30 +18,5 @@ public abstract class MemberCompactViewMapper extends RepresentationModelAssembl
     @Mapping(source = "registration", target = "registrationNumber")
     @Override
     abstract public MemberViewCompactApiDto toModel(Member entity);
-
-    @AfterMapping
-    void assembleLinks(Member entity, @MappingTarget MemberViewCompactApiDto target) {
-        target.add(linkTo(methodOn(MembersController.class).membersMemberIdGet(entity.getId())).withSelfRel());
-
-        if (securityService.canEditMemberData(entity.getId())) {
-            target.add(linkTo(methodOn(MembersController.class).membersMemberIdEditOwnMemberInfoFormGet(entity.getId())).withRel("members:editOwnInfo"));
-        }
-
-        if (securityService.hasGrant(ApplicationGrant.MEMBERS_EDIT)) {
-            target.add(linkTo(methodOn(MembersController.class).getMemberEditByAdminForm(entity.getId())).withRel("members:editAnotherMember"));
-        }
-    }
-
-
-    @Override
-    public MemberViewCompactApiDto convert(Member source) {
-        return toModel(source);
-    }
-
-    @Autowired
-    public void setSecurityService(KlabisSecurityService securityService) {
-        this.securityService = securityService;
-    }
-
-    ;
+    
 }
