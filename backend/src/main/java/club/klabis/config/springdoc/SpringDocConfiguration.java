8282b0f0296e160b0370bd635454c87ebd232917
David Polach <d.polach@dpolach.com>
2025-02-22 20:30:27 +0100

    Server shows generated OpenAPI specs from currently active endpoints.

---
+++ backend/src/main/java/club/klabis/config/springdoc/SpringDocConfiguration.java
@@ -0,0 +1,13 @@
+package club.klabis.config.springdoc;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@SecurityScheme(name = "security_auth", type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows(authorizationCode = @OAuthFlow(authorizationUrl = "/oauth/authorize", tokenUrl = "/oauth/token", scopes = {
+                //@OAuthScope(name = "openid", description = "openid scope")
+        })))
+public class SpringDocConfiguration {
+}
