fd4fe60b824f9224860a0d6ba2afae224ed1202b
David Polach <d.polach@dpolach.com>
2025-02-26 02:05:54 +0100

    Revert ""custom" URLs for some OAuth2 endpoints replaced with standard ones (both backend and frontend)"
    This reverts commit b09943699ec46bc1f5d8785c72187ec2c7d4c497.

--- backend/src/main/java/club/klabis/config/springdoc/SpringDocConfiguration.java
+++ backend/src/main/java/club/klabis/config/springdoc/SpringDocConfiguration.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @SecurityScheme(name = "security_auth", type = SecuritySchemeType.OAUTH2,
-        flows = @OAuthFlows(authorizationCode = @OAuthFlow(authorizationUrl = "/oauth2/authorize", tokenUrl = "/oauth2/token", scopes = {
+        flows = @OAuthFlows(authorizationCode = @OAuthFlow(authorizationUrl = "/oauth/authorize", tokenUrl = "/oauth/token", scopes = {
                 //@OAuthScope(name = "openid", description = "openid scope")
         })))
 public class SpringDocConfiguration {
