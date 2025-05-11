2fd1f53801a602fa6f1db26ea23bc8ab41f06c83
David Polach <d.polach@dpolach.com>
2025-05-11 22:24:38 +0200

    Upgraded springboot to 3.4.5

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -7,7 +7,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
     java
-    id("org.springframework.boot") version "3.3.4"
+    id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.5"
     id("org.openapi.generator") version "7.6.0"
 }
