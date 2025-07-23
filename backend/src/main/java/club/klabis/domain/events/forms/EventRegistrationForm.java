49e2a23efd0c4d4c2f18cdca4eaab92f3d9605bf
David Polach <d.polach@dpolach.com>
2025-07-23 12:10:31 +0200

    Record Builders for forms

--- backend/src/main/java/club/klabis/domain/events/forms/EventRegistrationForm.java
+++ backend/src/main/java/club/klabis/domain/events/forms/EventRegistrationForm.java
@@ -1,6 +1,8 @@
 package club.klabis.domain.events.forms;
 
 import club.klabis.domain.members.Member;
+import io.soabase.recordbuilder.core.RecordBuilder;
 
+@RecordBuilder
 public record EventRegistrationForm(Member.Id memberId, String siNumber) {
 }
