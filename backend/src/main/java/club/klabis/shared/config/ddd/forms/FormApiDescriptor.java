9db2067116b0bf445a86e3ffea4b895e3666390b
David Polach <d.polach@dpolach.com>
2025-08-30 00:57:43 +0200

    Removed form use case - won't use it here. (it was moved to own repository)

--- backend/src/main/java/club/klabis/shared/config/ddd/forms/FormApiDescriptor.java
+++
@@ -1,4 +0,0 @@
-package club.klabis.shared.config.ddd.forms;
-
-public record FormApiDescriptor<T>(String apiPath, FormHandler<T> formHandler, Class<T> formType) {
-}
--- backend/src/main/java/club/klabis/shared/config/ddd/forms/FormHandler.java
+++
@@ -1,8 +0,0 @@
-package club.klabis.shared.config.ddd.forms;
-
-public interface FormHandler<T> {
-    public T fetchFormData();
-
-    public void submitFormData(T formData) throws InvalidFormDataException;
-
-}
