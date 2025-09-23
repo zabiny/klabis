3dd2d905a9383a57fd1ec2fa67d61bb9cb19d86c
David Polach <d.polach@dpolach.com>
2025-09-23 22:11:19 +0200

    Use Formik for handling forms

--- frontend-2/src/components/form/index.ts
+++
@@ -1,4 +0,0 @@
-export {Form} from './Form.tsx';
-export {Field} from './Field.tsx';
-export type {FormProps, FieldProps, ValidationErrors} from './types.ts';
-export {getNestedValue, setNestedValue} from './utils.ts';
