ab54713af6abfd99a60b2de9f927f44fcfefa26b
David Polach <d.polach@dpolach.com>
2025-09-29 19:52:43 +0200

    Enable Modulithic support and exclude shared package from generated modulith documentation

--- backend/src/main/java/club/klabis/KlabisApplication.java
+++ backend/src/main/java/club/klabis/KlabisApplication.java
@@ -4,9 +4,11 @@ import club.klabis.shared.config.KlabisProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.modulith.Modulithic;
 
 @SpringBootApplication
 @EnableConfigurationProperties(KlabisProperties.class)
+@Modulithic
 public class KlabisApplication {
     public static void main(String[] args) {
         SpringApplication.run(KlabisApplication.class, args);
--- backend/src/test/java/club/klabis/Modulith.java
+++ backend/src/test/java/club/klabis/Modulith.java
@@ -1,12 +1,14 @@
 package club.klabis;
 
+import com.tngtech.archunit.core.domain.JavaClass;
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
 import org.springframework.modulith.docs.Documenter;
 
 public class Modulith {
 
-    ApplicationModules modules = ApplicationModules.of(KlabisApplication.class);
+    ApplicationModules modules = ApplicationModules.of(KlabisApplication.class,
+            JavaClass.Predicates.resideInAPackage("club.klabis.shared.."));
 
     // Generates documentation of modules into build/spring-modulith-docs
     @Test
