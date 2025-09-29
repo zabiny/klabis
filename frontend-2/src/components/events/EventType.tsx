c52eda5223047e22a78b81bc4989baee738cdef2
David Polach <d.polach@dpolach.com>
2025-09-29 16:57:59 +0200

    Add EventType component and use it for type column rendering in EventsPage, removing old label helpers.

---
+++ frontend-2/src/components/events/EventType.tsx
@@ -0,0 +1,31 @@
+import {ReactNode} from "react";
+
+type EventT = 'TRAINING' | 'COMPETITION';
+
+interface EventTypeProps {
+    eventType: EventT;
+}
+
+const translate = (name: EventT): ReactNode => {
+    if (!name) {
+        return "";
+    }
+    if (name == "TRAINING") {
+        return "Trénink";
+    }
+
+    if (name == "COMPETITION") {
+        return "Závod";
+    }
+
+    return "Neznámý typ";
+}
+
+function EventType({eventType}: EventTypeProps) {
+
+    return (
+        <span>{translate(eventType)}</span>
+    );
+}
+
+export default EventType;
--- frontend-2/src/pages/EventsPage.tsx
+++ frontend-2/src/pages/EventsPage.tsx
@@ -4,36 +4,13 @@ import {type EventListItem} from '../api/eventsApi';
 import {KlabisTable, TableCell} from "../components/KlabisTable";
 import MemberName from "../components/members/MemberName";
 import {Actions} from "../components/Actions";
+import EventType from "../components/events/EventType";
 
 const EventsPage = () => {
     // Function to format date
     const formatDate = (dateString: string) => {
         const date = new Date(dateString);
         return new Intl.DateTimeFormat('cs-CZ').format(date);
-    };
-
-    // Function to get event type label
-    const getEventTypeLabel = (type: string) => {
-        switch (type) {
-            case 'T':
-                return 'Trénink';
-            case 'S':
-                return 'Závod';
-            default:
-                return 'Neznamy';
-        }
-    };
-
-    // Function to get event type color
-    const getEventTypeColor = (type: string) => {
-        switch (type) {
-            case 'T':
-                return 'primary';
-            case 'S':
-                return 'secondary';
-            default:
-                return 'default';
-        }
     };
 
     return (
@@ -47,16 +24,14 @@ const EventsPage = () => {
                 <TableCell sortable column={"name"}>Název</TableCell>
                 <TableCell sortable column={"location"}>Místo</TableCell>
                 <TableCell sortable column={"organizer"}>Pořadatel</TableCell>
-                <TableCell column={"type"}>Typ</TableCell>
+                <TableCell column={"type"} dataRender={({value}) => <EventType eventType={value}/>}>Typ</TableCell>
                 <TableCell column={"web"} dataRender={({value}) => <Link href={value}><Public/></Link>}>Web</TableCell>
-                <TableCell sortable column={"registrationDeadline"}
-                           dataRender={({value}) => formatDate(value)}>Uzávěrka přihlášek</TableCell>
-                <TableCell sortable column={"coordinator"} dataRender={({value}) => value ?
+                <TableCell sortable column={"registrationDeadline"} dataRender={({value}) => formatDate(value)}>Uzávěrka
+                    přihlášek</TableCell>
+                <TableCell column={"coordinator"} dataRender={({value}) => value ?
                     <MemberName memberId={value}/> : <>--</>}>Vedoucí</TableCell>
-                <TableCell column="_actions"
-                           dataRender={props => (<></>)}>Možnosti</TableCell>
-                <TableCell column="_actions"
-                           dataRender={props => (<Actions value={props.value}/>)}>Akce</TableCell>
+                <TableCell column="_actions" dataRender={props => (<></>)}>Možnosti</TableCell>
+                <TableCell column="_actions" dataRender={props => (<Actions value={props.value}/>)}>Akce</TableCell>
             </KlabisTable>
         </Box>
     );
