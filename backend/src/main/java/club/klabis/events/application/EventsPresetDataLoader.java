8b3e6c3b95250adf1eb682067dd838b12a2ad6e6
David Polach <d.polach@dpolach.com>
2025-09-30 16:44:15 +0200

    Refactor EventsPresetDataLoader to use Stream API and proper LOG.info instead of System.out.printf.

--- backend/src/main/java/club/klabis/events/application/EventsPresetDataLoader.java
+++ backend/src/main/java/club/klabis/events/application/EventsPresetDataLoader.java
@@ -1,7 +1,6 @@
 package club.klabis.events.application;
 
 import club.klabis.PresetDataLoader;
-import club.klabis.events.domain.Event;
 import club.klabis.events.domain.forms.EventEditationForm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
+import java.util.stream.Stream;
 
 // load these data only when ORIS integration is disabled
 @ConditionalOnProperty(prefix = "oris-integration", name = "enabled", havingValue = "false", matchIfMissing = true)
@@ -26,21 +26,23 @@ class EventsPresetDataLoader implements PresetDataLoader {
 
     @Override
     public void loadData() {
-        Event createdEvent = eventsService.createNewEvent(new EventEditationForm("Example opened event",
-                "Brno",
-                LocalDate.now(),
-                "ZBM",
-                ZonedDateTime.now()
-                        .plusDays(3),
-                null));
-        System.out.printf("Created event with ID %s%n", createdEvent.getId());
-        createdEvent = eventsService.createNewEvent(new EventEditationForm("Example passed event",
-                "Jilemnice",
-                LocalDate.now().minusDays(12),
-                "ZBM",
-                ZonedDateTime.now()
-                        .minusDays(20),
-                null));
-        System.out.printf("Created event with ID %s%n", createdEvent.getId());
+        Stream.of(
+                        new EventEditationForm("Example opened event",
+                                "Brno",
+                                LocalDate.now(),
+                                "ZBM",
+                                ZonedDateTime.now()
+                                        .plusDays(3),
+                                null),
+                        new EventEditationForm("Example passed event",
+                                "Jilemnice",
+                                LocalDate.now().minusDays(12),
+                                "ZBM",
+                                ZonedDateTime.now()
+                                        .minusDays(20),
+                                null)
+                )
+                .map(eventsService::createNewEvent)
+                .forEach(createdEvent -> LOG.info("Created event with ID {}$n", createdEvent.getId()));
     }
 }
