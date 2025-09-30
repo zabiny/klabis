a6f7832102011d9c4d9c75e686f4bd1f7c2316ce
David Polach <d.polach@dpolach.com>
2025-09-30 15:21:01 +0200

    Add icons with tooltips to Actions component for better action representation

--- frontend-2/src/components/Actions.tsx
+++ frontend-2/src/components/Actions.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import {type ReactNode} from "react";
+import {Newspaper, SyncSharp} from "@mui/icons-material";
+import {Tooltip} from "@mui/material";
+
+const addTooltip = (text: string, node: ReactNode): ReactNode => {
+    return <Tooltip title={text}><>{node}</>
+    </Tooltip>;
+}
+
+const createIcon = (actionName: string): ReactNode => {
+    switch (actionName) {
+        case 'synchronize':
+            return addTooltip(actionName, <SyncSharp/>);
+        case 'createRegistration':
+            return addTooltip(actionName, <Newspaper/>);
+        default:
+            return <>{actionName}</>;
+    }
+}
 
 export const Actions = ({value}: { value: string[] }): React.ReactNode => {
     return (
-        <span>{value.map(s => <div>{s}</div>)}</span>
+        <span>{value.map(s => <div><Tooltip title={s}><span>{createIcon(s)}</span></Tooltip></div>)}</span>
     );
 }
