c200c9dc29e109d1e2e3c63bf3b756438dc22b89
David Polach <d.polach@dpolach.com>
2025-11-28 01:02:00 +0100

    Add SimpleCacheManager bean to CachingConfiguration - newly missing bean in Spring 7

--- backend/src/main/java/club/klabis/shared/config/CachingConfiguration.java
+++ backend/src/main/java/club/klabis/shared/config/CachingConfiguration.java
@@ -1,9 +1,16 @@
 package club.klabis.shared.config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableCaching
 class CachingConfiguration {
+    @Bean
+    public CacheManager cacheManager() {
+        return new SimpleCacheManager();
+    }
 }
