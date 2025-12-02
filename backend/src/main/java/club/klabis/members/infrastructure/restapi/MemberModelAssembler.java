43acdf4cbc5dbd4105374b846da307293deea1d3
David Polach <d.polach@dpolach.com>
2025-12-02 19:27:42 +0100

    Reorganized affordances for Member details resource

--- backend/src/main/java/club/klabis/members/infrastructure/restapi/MemberModelAssembler.java
+++ backend/src/main/java/club/klabis/members/infrastructure/restapi/MemberModelAssembler.java
@@ -7,13 +7,14 @@ import club.klabis.shared.config.hateoas.ModelPreparator;
 import club.klabis.shared.config.security.ApplicationGrant;
 import club.klabis.shared.config.security.KlabisSecurityService;
 import club.klabis.users.infrastructure.restapi.UserPermissionsApi;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.*;
 import org.springframework.hateoas.server.EntityLinks;
-import org.springframework.hateoas.server.LinkRelationProvider;
 import org.springframework.stereotype.Component;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import java.util.ArrayList;
+import java.util.List;
+
+import static club.klabis.shared.config.hateoas.forms.KlabisHateoasImprovements.affordBetter;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
@@ -22,13 +23,11 @@ public class MemberModelAssembler implements ModelPreparator<Member, MembersApiR
     private final ConversionService conversionService;
     private final KlabisSecurityService securityService;
     private final EntityLinks entityLinks;
-    private final LinkRelationProvider linkRelationProvider;
 
-    public MemberModelAssembler(ConversionService conversionService, KlabisSecurityService securityService, EntityLinks entityLinks, LinkRelationProvider linkRelationProvider) {
+    public MemberModelAssembler(ConversionService conversionService, KlabisSecurityService securityService, EntityLinks entityLinks) {
         this.conversionService = conversionService;
         this.securityService = securityService;
         this.entityLinks = entityLinks;
-        this.linkRelationProvider = linkRelationProvider;
     }
 
     @Override
@@ -39,49 +38,63 @@ public class MemberModelAssembler implements ModelPreparator<Member, MembersApiR
 
     @Override
     public void addLinks(EntityModel<MembersApiResponse> target, Member entity) {
-        target.add(entityLinks.linkToItemResource(Member.class, entity.getId().value()).withSelfRel());
-        target.add(entityLinks.linkToCollectionResource(Member.class)
-                .withRel(linkRelationProvider.getCollectionResourceRelFor(
-                        Member.class)));
+        List<Affordance> selfAffordances = new ArrayList<>();
 
         if (entity.isSuspended()) {
             if (securityService.hasGrant(ApplicationGrant.MEMBERS_SUSPENDMEMBERSHIP)) {
-                target.add(linkTo(methodOn(SuspendMemberUseCaseControllers.class).resumeMembership(entity.getId())).withRel(
-                        ApplicationGrant.MEMBERS_RESUMEMEMBERSHIP.getGrantName()).withName("Obnovit členství"));
+                selfAffordances.add(affordBetter(methodOn(SuspendMemberUseCaseControllers.class).resumeMembership(entity.getId())));
+//                target.add(linkTo(methodOn(SuspendMemberUseCaseControllers.class).resumeMembership(entity.getId())).withRel(
+//                        ApplicationGrant.MEMBERS_RESUMEMEMBERSHIP.getGrantName()).withName("Obnovit členství"));
             }
-            return;
-        }
+        } else {
 
-        if (securityService.canEditMemberData(entity.getId())) {
-            target.add(linkTo(methodOn(EditOwnInfoUseCaseControllers.class).membersMemberIdEditOwnMemberInfoFormGet(
-                    entity.getId())).withRel("members:editOwnInfo").withName("Upravit moje údaje"));
-        }
+            if (securityService.canEditMemberData(entity.getId())) {
+                selfAffordances.add(affordBetter(methodOn(EditOwnInfoUseCaseControllers.class).membersMemberIdEditOwnMemberInfoFormPut(
+                        entity.getId(), null)));
+//            target.add(linkTo(methodOn(EditOwnInfoUseCaseControllers.class).membersMemberIdEditOwnMemberInfoFormGet(
+//                    entity.getId())).withRel("members:editOwnInfo").withName("Upravit moje údaje"));
+            }
 
-        if (securityService.hasGrant(ApplicationGrant.MEMBERS_EDIT)) {
-            target.add(linkTo(methodOn(AdminMemberEditUseCaseControllers.class).getMemberEditByAdminForm(entity.getId())).withRel(
-                    ApplicationGrant.MEMBERS_EDIT.getGrantName()).withName("Upravit údaje člena klubu"));
-        }
+            if (securityService.hasGrant(ApplicationGrant.MEMBERS_EDIT)) {
+                selfAffordances.add(affordBetter(methodOn(AdminMemberEditUseCaseControllers.class).putMemberEditByAdminForm(
+                        entity.getId(), null)));
+//            target.add(linkTo(methodOn(AdminMemberEditUseCaseControllers.class).getMemberEditByAdminForm(entity.getId())).withRel(
+//                    ApplicationGrant.MEMBERS_EDIT.getGrantName()).withName("Upravit údaje člena klubu"));
+            }
 
-        if (securityService.hasGrant(ApplicationGrant.MEMBERS_SUSPENDMEMBERSHIP)) {
-            target.add(linkTo(methodOn(SuspendMemberUseCaseControllers.class,
-                    entity.getId()).membersMemberIdSuspendMembershipFormGet(
-                    entity.getId())).withRel(ApplicationGrant.MEMBERS_SUSPENDMEMBERSHIP.getGrantName())
-                    .withName("Pozastavit členství"));
-        }
+            if (securityService.hasGrant(ApplicationGrant.MEMBERS_SUSPENDMEMBERSHIP)) {
+                selfAffordances.add(affordBetter(methodOn(SuspendMemberUseCaseControllers.class,
+                        entity.getId()).membersMemberIdSuspendMembershipFormPut(
+                        entity.getId(), null)));
+//            target.add(linkTo(methodOn(SuspendMemberUseCaseControllers.class,
+//                    entity.getId()).membersMemberIdSuspendMembershipFormGet(
+//                    entity.getId())).withRel(ApplicationGrant.MEMBERS_SUSPENDMEMBERSHIP.getGrantName())
+//                    .withName("Pozastavit členství"));
+            }
 
-        if (securityService.hasGrant(ApplicationGrant.APPUSERS_PERMISSIONS)) {
-            entity.getAppUserId()
-                    .ifPresent(appUserId -> target.add(linkTo(methodOn(UserPermissionsApi.class).getUserGrants(
-                            appUserId)).withRel(
-                            ApplicationGrant.APPUSERS_PERMISSIONS.getGrantName()).withName("Upravit oprávnění")));
+            if (securityService.hasGrant(ApplicationGrant.APPUSERS_PERMISSIONS)) {
+                entity.getAppUserId()
+                        .ifPresent(appUserId -> selfAffordances.add(affordBetter(methodOn(UserPermissionsApi.class).updateMemberGrants(
+                                appUserId, null))));
+            }
         }
+        target.add(entityLinks.linkToItemResource(Member.class, entity.getId().value())
+                .withSelfRel()
+                .andAffordances(selfAffordances));
+    }
+
+    private void removeSelfAffordances(RepresentationModel<?> resourceModel) {
+        resourceModel.mapLink(LinkRelation.of("self"), Link::withoutAffordances);
     }
 
     @Override
     public void addLinks(CollectionModel<EntityModel<MembersApiResponse>> model) {
+        model.getContent().forEach(this::removeSelfAffordances);
+
         if (securityService.hasGrant(ApplicationGrant.MEMBERS_REGISTER)) {
-            model.add(linkTo(methodOn(RegisterNewMemberController.class).memberRegistrationsPost(null))
-                    .withRel(ApplicationGrant.MEMBERS_REGISTER.getGrantName()).withName("Nový člen klubu"));
+            model.mapLink(LinkRelation.of("self"),
+                    link -> link.andAffordance(affordBetter(methodOn(RegisterNewMemberController.class).memberRegistrationsPost(
+                            null))));
         }
     }
 
