27806057d5620f23d51bbd28a1d77da1bfcfc2a0
David Polach <d.polach@dpolach.com>
2025-12-05 20:11:05 +0100

    Added link to "My info" into root links

--- backend/src/main/java/club/klabis/members/infrastructure/restapi/MembersRootPostprocessor.java
+++ backend/src/main/java/club/klabis/members/infrastructure/restapi/MembersRootPostprocessor.java
@@ -2,6 +2,7 @@ package club.klabis.members.infrastructure.restapi;
 
 import club.klabis.members.domain.Member;
 import club.klabis.shared.config.hateoas.RootModel;
+import club.klabis.shared.config.security.KlabisSecurityService;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.EntityLinks;
 import org.springframework.hateoas.server.LinkRelationProvider;
@@ -12,14 +13,20 @@ import org.springframework.stereotype.Component;
 class MembersRootPostprocessor implements RepresentationModelProcessor<EntityModel<RootModel>> {
     private final EntityLinks entityLinks;
     private final LinkRelationProvider linkRelationProvider;
+    private final KlabisSecurityService securityService;
 
-    public MembersRootPostprocessor(EntityLinks entityLinks, LinkRelationProvider linkRelationProvider) {
+    public MembersRootPostprocessor(EntityLinks entityLinks, LinkRelationProvider linkRelationProvider, KlabisSecurityService securityService) {
         this.entityLinks = entityLinks;
         this.linkRelationProvider = linkRelationProvider;
+        this.securityService = securityService;
     }
 
     @Override
     public EntityModel<RootModel> process(EntityModel<RootModel> model) {
+        securityService.getAuthenticatedMemberId()
+                .ifPresent(memberId -> model.add(entityLinks.linkToItemResource(Member.class, memberId)
+                        .withRel("myInfo")));
+
         model.add(entityLinks.linkToCollectionResource(Member.class)
                 .withRel(linkRelationProvider.getCollectionResourceRelFor(Member.class)));
 
