69639d24bac6c04ecbc42a0e45c3ff9e3564c0e5
David Polach <d.polach@dpolach.com>
2025-12-05 19:14:49 +0100

    Removed overcomplicated classes generated from OpenAPI

--- backend/src/main/java/club/klabis/users/infrastructure/restapi/UserPermissionsApiController.java
+++ backend/src/main/java/club/klabis/users/infrastructure/restapi/UserPermissionsApiController.java
@@ -10,8 +10,6 @@ import club.klabis.users.application.ApplicationUserNotFound;
 import club.klabis.users.application.ApplicationUsersRepository;
 import club.klabis.users.application.UserGrantsUpdateUseCase;
 import club.klabis.users.domain.ApplicationUser;
-import club.klabis.users.infrastructure.restapi.dto.GetAllGrants200ResponseApiDto;
-import club.klabis.users.infrastructure.restapi.dto.GlobalGrantDetailApiDto;
 import club.klabis.users.infrastructure.restapi.dto.MemberGrantsFormApiDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -21,7 +19,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
-import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.hateoas.Affordance;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
@@ -34,7 +31,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.Collection;
 import java.util.List;
 
 import static club.klabis.shared.config.hateoas.forms.KlabisHateoasImprovements.affordBetter;
@@ -54,6 +50,14 @@ public class UserPermissionsApiController {
         this.applicationUsersRepository = applicationUsersRepository;
     }
 
+    public record HalFormsOptionItem<T>(T value, String prompt) {
+
+    }
+
+    private HalFormsOptionItem<ApplicationGrant> fromGrant(ApplicationGrant grant) {
+        return new HalFormsOptionItem<>(grant, grant.getDescription());
+    }
+
     /**
      * GET /grants : returns details about available security grants what can be assigned to users
      *
@@ -61,35 +65,10 @@ public class UserPermissionsApiController {
      */
     @Operation(
             operationId = "getAllGrants",
-            summary = "returns details about available security grants what can be assigned to users",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "List of grants which can be assigned to members", content = {
-                            @Content(mediaType = "application/json", schema = @Schema(implementation = GetAllGrants200ResponseApiDto.class))
-                    })
-            }
+            summary = "returns details about available security grants what can be assigned to users"
     )
-    @GetMapping(
-            value = "/grants",
-            produces = {"application/json"}
-    )
-    public ResponseEntity<GetAllGrants200ResponseApiDto> getAllGrants() {
-        Collection<ApplicationGrant> globalGrants = ApplicationGrant.globalGrants();
-        List<GlobalGrantDetailApiDto> convertedGrants = conversionService.convert(
-                globalGrants,
-                TypeDescriptor.collection(List.class, TypeDescriptor.valueOf(GlobalGrantDetailApiDto.class)));
-        return ResponseEntity.ok(GetAllGrants200ResponseApiDto.builder().grants(convertedGrants).build());
-    }
-
-    public record HalFormsOptionItem(String value, String prompt) {
-
-    }
-
-    private HalFormsOptionItem fromGrant(ApplicationGrant grant) {
-        return new HalFormsOptionItem(grant.getGrantName(), grant.getDescription());
-    }
-
-    @GetMapping(value = "/grant_options")
-    public List<HalFormsOptionItem> getAllGrantsOptions() {
+    @GetMapping(value = "/grants")
+    public List<HalFormsOptionItem<ApplicationGrant>> getAllGrantsOptions() {
         return ApplicationGrant.globalGrants().stream().map(this::fromGrant).toList();
     }
 
@@ -114,8 +93,7 @@ public class UserPermissionsApiController {
         ApplicationUser appUser = applicationUsersRepository.findById(userId)
                 .orElseThrow(() -> ApplicationUserNotFound.forUserId(userId));
 
-        return ResponseEntity.ok(conversionService.convert(appUser,
-                MemberGrantsFormApiDto.class));
+        return ResponseEntity.ok(new MemberGrantsFormApiDto(appUser.getGlobalGrants()));
     }
 
     @Operation(
@@ -132,10 +110,7 @@ public class UserPermissionsApiController {
             @Parameter(name = "userId", description = "ID of application user", required = true, in = ParameterIn.PATH) @PathVariable("userId") ApplicationUser.Id userId,
             @Parameter(name = "MemberGrantsFormApiDto", description = "") @Valid @RequestBody(required = false) MemberGrantsFormApiDto memberGrantsFormApiDto
     ) {
-        Collection<ApplicationGrant> globalGrants = conversionService.convert(
-                memberGrantsFormApiDto.grants(),
-                TypeDescriptor.collection(List.class, TypeDescriptor.valueOf(ApplicationGrant.class)));
-        userGrantsUpdateUseCase.setGlobalGrants(userId, globalGrants);
+        userGrantsUpdateUseCase.setGlobalGrants(userId, memberGrantsFormApiDto.grants());
         return ResponseEntity.ok(null);
     }
 }
--- backend/src/main/java/club/klabis/users/infrastructure/restapi/dto/MemberGrantsFormApiDto.java
+++ backend/src/main/java/club/klabis/users/infrastructure/restapi/dto/MemberGrantsFormApiDto.java
@@ -1,13 +1,14 @@
 package club.klabis.users.infrastructure.restapi.dto;
 
 import club.klabis.shared.config.hateoas.KlabisInputTypes;
+import club.klabis.shared.config.security.ApplicationGrant;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import io.soabase.recordbuilder.core.RecordBuilder;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import org.springframework.hateoas.InputType;
 
-import java.util.List;
+import java.util.Collection;
 
 /**
  * Data for form setting member grants
@@ -19,7 +20,7 @@ import java.util.List;
 public record MemberGrantsFormApiDto(
         @InputType(KlabisInputTypes.CHECKBOX_INPUT_TYPE)
         @Valid
-        List<GlobalGrantsApiDto> grants
+        Collection<ApplicationGrant> grants
 
 ) {
 
