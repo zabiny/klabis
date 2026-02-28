316b9c957335cb4ecee4130e9a42f8b7144901b2
David Polach <d.polach@dpolach.com>
2026-02-28 03:35:19 +0100

    docs(test): add inline documentation for WithKlabisMockUser annotation fields
    - Explain purpose of `userId`, `memberId`, `username`, and `authorities` attributes.

--- backend/src/test/java/com/klabis/common/WithKlabisMockUser.java
+++ backend/src/test/java/com/klabis/common/WithKlabisMockUser.java
@@ -15,11 +15,15 @@ import java.lang.annotation.Target;
 )
 public @interface WithKlabisMockUser {
 
+    // defines userId. If missing, random value is used as user Id.
     String userId() default "";
 
+    // Defines memberId for user (if not defined, it's considered that user doesn't have Member instance). If memberId is defined and userId is missing, then same value is used as userId
     String memberId() default "";
 
+    // affects Authentication.getName.
     String username() default "";
 
+    // authorities what will be available for authenticated user. Allows to easily test authorizations of the API endpoints.
     Authority[] authorities() default {};
 }
